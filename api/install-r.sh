61b5989de53638d3d7178280e62ad528c3883117
Yaodong Zhao <yaodong.zhao@utah.edu>
2017-11-03 02:41:26 -0600

    Fix the issue that TDA 1.5.1 can't be installed on Ubuntu

--- api/install-r.sh
+++ api/install-r.sh
@@ -5,7 +5,7 @@
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
-add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
+add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu zesty/'
 
 apt-get -qq update
 
--- api/install.r
+++ api/install.r
@@ -1,3 +1,6 @@
 #!/usr/bin/env Rscript
 
-install.packages("TDA", repos = "http://cran.us.r-project.org")
+install.packages("devtools", repos = "https://cran.rstudio.org/")
+require(devtools);
+install_version('igraph', version='1.0.1', repos='https://cran.rstudio.org')
+install_version('TDA', version='1.4.1', repos='https://cran.rstudio.org')
